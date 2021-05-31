325c266689bd20a57b37824ba8aedcdb97ae2d5d
Dominik Bucher <d.bucher@centrum.cz>
2021-05-31 14:33:21 +0200

    Add scripts for generating documentation

---
+++ Scripts/addImagesToDocumentation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Move to project root directory
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$scriptDir/../"
+
+DOCUMENTATION_FOLDER=$1
+PATH_TO_SNAPSHOTS="UISDKdocumentation/__Snapshots__"
+
+for UI_SNAPSHOT in ${PATH_TO_SNAPSHOTS}/*;do
+    STRIPPED_PATH=`basename $UI_SNAPSHOT`
+    COMPONENT_NAME=${STRIPPED_PATH%_*_*}
+    
+    DOCUMENTATION_FILE=`find $DOCUMENTATION_FOLDER -name "_$COMPONENT_NAME.md"`
+    # Let's use just light variation of the screenshot, we can support dark mode later.
+    FINAL_SNAPSHOT=`ls $UI_SNAPSHOT | grep light`
+    tail -1 "$DOCUMENTATION_FILE" | grep "$FINAL_SNAPSHOT"
+    if [ $? -eq 0 ];then
+        echo "There is already line containing the snapshot for $COMPONENT_NAME, skipping adding of documentation."
+        continue
+    fi
+    
+    echo "Adding snapshot of $COMPONENT_NAME to documentation..."
+    
+    SNAPSHOT_ANNOTATION_TEXT="![$COMPONENT_NAME](/$UI_SNAPSHOT/$FINAL_SNAPSHOT)"
+    echo -e "$SNAPSHOT_ANNOTATION_TEXT\n$(cat $DOCUMENTATION_FILE)" > $DOCUMENTATION_FILE
+    
+done
---
+++ Scripts/generateDocumentation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+TARGET=$1
+
+# Move to project root directory
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$scriptDir/../"
+
+if [[ "$TARGET" = "StreamChat" ]];then
+    TARGET_DIRECTORY="Sources/StreamChat"
+elif [[ "$TARGET" = "StreamChatUI" ]]; then
+    TARGET_DIRECTORY="Sources/StreamChatUI"
+else
+    echo "Please specify target to generate docs for (StreamChat or StreamChatUI)"
+    exit 1
+fi
+
+OUTPUT_DIRECTORY="Documentation"
+
+IGNORED_PATHS='Resources\|__Snapshots__\|Tests\|Generated'
+
+# As of May 2021, swift-doc doesn't support directories, so we find all the subdirectories instead
+find "$TARGET_DIRECTORY" -type d -print | grep -v -e $IGNORED_PATHS > /tmp/StreamChat_folder_directories.txt
+
+# Let's run the documentation inside all subdirectories in the target and directory so we can mirror the directories structure
+# However SwiftDoc generates from the root, that means `Sources/$TARGET/Component.md` instead of just `$TARGET/Component.md`
+while read DIRECTORY; do
+  swift doc generate $DIRECTORY -n $TARGET -o "$OUTPUT_DIRECTORY/$DIRECTORY"
+done < /tmp/StreamChat_folder_directories.txt
+
+# cleanup the duplicate files by comparing what is not in the Sources directory.
+bash Scripts/deleteDuplicates.sh "$OUTPUT_DIRECTORY/$TARGET_DIRECTORY" "$TARGET_DIRECTORY"
+
+# Delete first lines in files
+find "$OUTPUT_DIRECTORY/$TARGET_DIRECTORY" -type f -exec sed -i '' '1d' {} +
+
+# Sed is no good when it comes with iterating files, the macOS version of sed doesn't make it any easier..
+# so we push to target directory to remove all the
+#pushd "$OUTPUT_DIRECTORY/$TARGET_DIRECTORY"
+#sed -i '' '1d' *.md
+#tail -n +2
+#popd
+
+if [[ "$TARGET" = "StreamChatUI" ]]; then
+    bash Scripts/addImagesToDocumentation.sh "$OUTPUT_DIRECTORY/Sources/StreamChatUI"
+fi
+
+echo "Documentation for $TARGET generated successfully. Please do check stream-chat-swift-docs/docs/Sources folder"
